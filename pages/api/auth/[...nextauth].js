3c37ff4d85c079c6d48df55df6d56cd31a86b2aa
haris96 <haris@fit-outfit.com>
2022-05-09 19:50:44 +0500

    still working with next-auth github signing in

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,11 +1,11 @@
 import NextAuth from 'next-auth'
-import Providers from 'next-auth/providers'
+import GithubProvider from 'next-auth/providers/github'
 
 export default NextAuth({
   providers: [
-    Providers.GitHub({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET
     })
   ]
 })
